170bffda32b3bb764301af4a250cf3f608cfdde9
cramosh-droid <cramosh@coflex.com.mx>
2026-07-02 22:27:30 +0000

    Inicio

---
+++ .vscode/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import panda as pd 
+st.set_page_config(
+    page_title="Dashboard Capital Humano"
+    layout="wide"
+)
+# titulo
+st.title("Dashboard Capital Humano")
+# Descripcion
+st.markdown(
+    """
+    Bienvenido al Dashboard de Capital Humano!
+    Esta herramienta fue creada con la finalidad de poder esplorar el talento interno de nuestra empresa.
+    Aqui observaras indicadores claves para el desarrollo de nuestros colaboradores!
+    """
+)
+st.divider()
